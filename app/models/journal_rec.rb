119aaadb1332cd42d532ed03dfadbd6360673e9e
TymofiiPys <nfstimoha2004@gmail.com>
2023-12-01 03:54:17 +0200

    omg database finally works but it's sqlite because i have no time for trickery with mysql

--- app/models/journal_rec.rb
+++ app/models/journal_rec.rb
@@ -1,2 +1,9 @@
 class JournalRec < ApplicationRecord
+  def average
+    sum = 0
+    sum += mark1.nil? ? 0 : mark1
+    sum += mark2.nil? ? 0 : mark2
+    sum += mark3.nil? ? 0 : mark3
+    (sum / 3).round(2)
+  end
 end
